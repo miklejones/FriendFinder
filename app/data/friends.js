8d6c52c2f04ead1f7b158069d2677a9b39eb5384
Michael Garland <michaelcgarland@gmail.com>
2018-05-12 20:40:37 -0400

    README complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friends = [
     {
-        name:"Karl",
-        photo:"https://images.gr-assets.com/authors/1523865402p5/7084.jpg",
-        scores:[
+        name: "Karl",
+        photo: "https://images.gr-assets.com/authors/1523865402p5/7084.jpg",
+        scores: [
             1,
             5,
             3,
@@ -13,12 +13,12 @@ var friends = [
             5,
             1,
             1
-          ]
-      },
-      {
-        name:"Vlad",
-        photo:"http://spartacus-educational.com/00lenin2.jpg",
-        scores:[
+        ]
+    },
+    {
+        name: "Vlad",
+        photo: "http://spartacus-educational.com/00lenin2.jpg",
+        scores: [
             1,
             5,
             3,
@@ -29,8 +29,24 @@ var friends = [
             5,
             1,
             1
-          ]
-      }
-  ];
+        ]
+    },
+    {
+        name: "Leon",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/LeTrotskyDB_%28original%29.jpg/220px-LeTrotskyDB_%28original%29.jpg",
+        scores: [
+            1,
+            5,
+            3,
+            2,
+            1,
+            1,
+            1,
+            5,
+            1,
+            1
+        ]
+    }
+];
 
-  module.exports = friends;
+module.exports = friends;
