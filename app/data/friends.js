2e02404957e471595c56130ad0c13ae853a9233c
Michael Garland <michaelcgarland@gmail.com>
2018-05-12 22:37:06 -0400

    player stats

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,32 +19,32 @@ var friends = [
         name: "Vlad",
         photo: "http://spartacus-educational.com/00lenin2.jpg",
         scores: [
-            1,
-            5,
-            3,
             2,
+            3,
             1,
+            2,
+            4,
             1,
-            1,
+            2,
             5,
             1,
-            1
+            2
         ]
     },
     {
         name: "Leon",
         photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/LeTrotskyDB_%28original%29.jpg/220px-LeTrotskyDB_%28original%29.jpg",
         scores: [
-            1,
-            5,
-            3,
             2,
+            4,
+            3,
             1,
-            1,
-            1,
-            5,
-            1,
-            1
+            2,
+            2,
+            2,
+            4,
+            2,
+            2
         ]
     }
 ];
